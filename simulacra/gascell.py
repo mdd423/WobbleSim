fb07207db76c5e4b5dbb5293f128ebd822712024
MattD <matthewdaunt@obsolete.local>
2021-10-20 18:34:58 -0400

    gas cell update

--- simulacra/gascell.py
+++ simulacra/gascell.py
@@ -10,16 +10,14 @@ def read_in_idl(filename='data/gascell/keck_fts_renorm.idl'):
     return transmission, wavelength
 
 class GasCellModel(TheoryModel):
-    def __init__(self,filename):
+    def __init__(self,filename='../data/gascell/keck_fts_renorm.idl'):
         self.filename = filename
         transmission, wavelength = read_in_idl(self.filename)
         self.flux = transmission
-        self.wave = wavelength   * u.Angstrom
+        self.wave = wavelength * u.Angstrom
         self.color = 'green'
 
-    def generate_transmission(self,epoches):
-        self.flux = np.repeat(np.expand_dims(self.flux,axis=0),epoches,axis=0)
-        self.wave = np.repeat(np.expand_dims(self.wave,axis=0),epoches,axis=0)
-        # print('gas flux',self.flux.shape)
-        # print('gas wave',self.wave.shape)
+    def generate_transmission(self,times):
+        self.flux = np.repeat(np.expand_dims(self.flux,axis=0),times.shape[0],axis=0)
+        self.wave = np.repeat(np.expand_dims(self.wave,axis=0),times.shape[0],axis=0)
         return self.flux, self.wave

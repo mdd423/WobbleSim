ba58834b1c9f441e21ebc94891ec15a750a996e2
Matthew Daunt <mdd423@nyu.edu>
2025-06-19 15:59:00 -0400

    fix exit :q

--- simulacra/detector.py
+++ simulacra/detector.py
@@ -4,6 +4,8 @@ import astropy.constants as const
 import astropy.time as at
 import astropy.coordinates as coord
 
+import sys
+
 import scipy.interpolate as interp
 import scipy.ndimage as img
 import scipy.sparse
@@ -138,7 +140,7 @@ class Detector:
         self.stellar_model = stellar_model
         self.transmission_models = []
 
-        # Randomized Parameters
+        # Lanczos Parameters
         self.a       = a
 
         # Simulation Parameters
@@ -283,7 +285,7 @@ class Detector:
             }
         '''
 
-        if len(self.wave_grid) != 0:
+        if len(self.wave_grid) == 0:
             print('wave_grid is empty')
             sys.exit()
         data = DetectorData()

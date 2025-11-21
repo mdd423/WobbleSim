839767a8a4d2f6c5317b43f6ddcf3744ba0f85e6
Matthew Daunt <mdd423@nyu.edu>
2025-11-21 17:51:01 -0500

    bug

--- simulacra/detector.py
+++ simulacra/detector.py
@@ -419,7 +419,7 @@ class Detector:
         # Interpolate using Lanczos and Add Noise
         ##################################################
         print("interpolating data...")
-        P_exp = self.interpolate_data(x_hat, xs, f_lsf, new_step_size)
+        P_exp = self.interpolate_data(x_hat, xs, f_lsf, new_step_size) * flux_unit
 
         w_hat = np.exp(x_hat) * u.Angstrom
 
